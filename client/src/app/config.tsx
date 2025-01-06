94675d3fe0f47fe2c4eebfe00f4c8794d6dd48a7
Sathya Venugopal <sathya.venugopal2003@gmail.com>
2025-01-05 21:00:01 -0800

    Change backend to render

--- client/src/app/config.tsx
+++ client/src/app/config.tsx
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001"
-    : "https://movie-recommender-backend-nine.vercel.app";
+    : "https://movie-recommender-r3p9.onrender.com";
